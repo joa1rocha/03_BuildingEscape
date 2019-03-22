9f8aa979829fa02fc182475a5aa2c69f38e8d284
Joao <joa1.rocha@gmail.com>
2019-03-22 22:53:14 +0000

    pre holiday commit

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -60,8 +60,28 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		10.f
 	);
 
-	// Ray-cast out to reach distance
+	// Setup query params
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
 
-	// See what we hit
+	// line-trace (AKA ray-cast) out to reach distance
+	FHitResult Hit;
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+
+	AActor* ActorHit = Hit.GetActor();
+
+	if (ActorHit)
+	{
+		// See what we hit
+		UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"),
+			*ActorHit->GetName()
+		);
+	}
 }
 
